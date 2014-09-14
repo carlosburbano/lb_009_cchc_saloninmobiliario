ad86f2c8c9e0e9179c40df94625c9e4644562623
denis <denis@bitbang.cl>
2014-09-14 12:17:02 -0300

    footer con over

--- include/footer.php
+++ include/footer.php
@@ -21,13 +21,19 @@
 								<h4>Media Partners:</h4>
 								<div class="marcas">
 									<div class="col-xs-5">
-										<img class="img-responsive" src="img/multimedios.png" alt="">
+										<a href="http://www.multimediosglp.cl/" target="_blank"> 
+											<img class="img-responsive" src="img/multimedios.png" alt="">
+										</a>
 									</div>
 									<div class="col-xs-3">
-										<img class="img-responsive" src="img/mega.png" alt="">
+										<a href="http://www.mega.cl/" target="_blank"> 
+											<img class="img-responsive" src="img/mega.png" alt="">
+										</a>
 									</div>
 									<div class="col-xs-4">
-										<img class="img-responsive" src="img/latercera.png" alt="">
+										<a href="http://www.latercera.com/" target="_blank"> 
+											<img class="img-responsive" src="img/latercera.png" alt="">
+										</a>
 									</div>
 								</div>	
 							</div>
@@ -35,16 +41,24 @@
 								<h4>Media Partners:</h4>
 								<div class="marcas">
 									<div class="col-xs-3">
-										<img class="img-responsive" src="img/scotiabank.png" alt="">
+										<a href="http://www.scotiabankchile.cl/" target="_blank"> 
+											<img class="img-responsive" src="img/scotiabank.png" alt="">
+										</a>
 									</div>	
 									<div class="col-xs-3">
-										<img class="img-responsive" src="img/bci.png" alt="">
+										<a href="http://www.bci.cl/" target="_blank"> 
+											<img class="img-responsive" src="img/bci.png" alt="">
+										</a>
 									</div>	
 									<div class="col-xs-3">
-										<img class="img-responsive" src="img/bancoestado.png" alt="">
+										<a href="http://www.bancoestado.cl/" target="_blank"> 
+											<img class="img-responsive" src="img/bancoestado.png" alt="">
+										</a>
 									</div>	
 									<div class="col-xs-3">
-										<img class="img-responsive" src="img/abcdin.png" alt="">
+										<a href="http://www.abcdin.cl/" target="_blank"> 
+											<img class="img-responsive" src="img/abcdin.png" alt="">
+										</a>
 									</div>	
 								</div>	
 
@@ -53,7 +67,9 @@
 								<h4>Organiza:</h4>
 								<div class="marcas">
 									<div class="col-xs-4 col-sm-12">
-										<img class="img-responsive" src="img/logo_cchc.png" alt="">
+										<a href="http://www.cchc.cl/" target="_blank"> 	
+											<img class="img-responsive" src="img/logo_cchc.png" alt="">
+										</a>
 									</div>	
 								</div>	
 							</div>
